0b02a3943c6a1e8b547f7cffc39c49cbb8a987b0
cloudvillager <72429990+cloudvillager@users.noreply.github.com>
2026-09-13 22:06:27 +0800

    chore: 更新导航数据 - 2026/9/13 22:06:26

--- src/mock/user_data/cloudvillager__mao_nav.js
+++ src/mock/user_data/cloudvillager__mao_nav.js
@@ -26,20 +26,6 @@ export const mockData = {
           "url": "https://memos.norve.top/",
           "description": "",
           "icon": "https://img.norve.top/file/life/1789308084607_image.webp"
-        },
-        {
-          "id": "site-1789307817161",
-          "name": "豆包",
-          "url": "https://www.doubao.com/",
-          "description": "",
-          "icon": "https://img.norve.top/file/life/1789307805368_image.webp"
-        },
-        {
-          "id": "site-1789307867393",
-          "name": "deepseek",
-          "url": "https://www.deepseek.com/",
-          "description": "",
-          "icon": "https://www.faviconextractor.com/favicon/www.deepseek.com"
         }
       ]
     },
@@ -99,7 +85,29 @@ export const mockData = {
       "icon": "🌐",
       "name": "AI",
       "order": 3,
-      "sites": []
+      "sites": [
+        {
+          "id": "site-1789307817161",
+          "name": "豆包",
+          "url": "https://www.doubao.com/",
+          "description": "",
+          "icon": "https://img.norve.top/file/life/1789307805368_image.webp"
+        },
+        {
+          "id": "site-1789307867393",
+          "name": "deepseek",
+          "url": "https://www.deepseek.com/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/www.deepseek.com"
+        },
+        {
+          "id": "site-1789308383866",
+          "name": "halo",
+          "url": "https://ruwbzaesjt.pdrapi.ccwu.cc/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/ruwbzaesjt.pdrapi.ccwu.cc"
+        }
+      ]
     }
   ],
   "title": "猫猫导航🐱",

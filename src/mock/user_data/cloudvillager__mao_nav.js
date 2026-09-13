e3c8f9d6324ca148cc9f71b1f5607cd3e3813175
cloudvillager <72429990+cloudvillager@users.noreply.github.com>
2026-09-13 21:37:33 +0800

    chore: 更新导航数据 - 2026/9/13 21:37:32

--- src/mock/user_data/cloudvillager__mao_nav.js
+++ src/mock/user_data/cloudvillager__mao_nav.js
@@ -2,8 +2,8 @@ export const mockData = {
   "categories": [
     {
       "id": "category-1789306192968",
-      "icon": "📚",
-      "name": "我的常用",
+      "icon": "💻",
+      "name": "办公",
       "order": 0,
       "sites": [
         {
@@ -19,8 +19,29 @@ export const mockData = {
           "url": "https://worktodo.norve.top/",
           "description": "待办事项（工作）",
           "icon": "https://www.faviconextractor.com/favicon/worktodo.norve.top"
+        },
+        {
+          "id": "site-1789306552278",
+          "name": "图床",
+          "url": "https://img.norve.top/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/img.norve.top"
+        },
+        {
+          "id": "site-1789306574011",
+          "name": "备忘录",
+          "url": "https://memos.norve.top/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/memos.norve.top"
         }
       ]
+    },
+    {
+      "id": "category-1789306635606",
+      "icon": "🛠️",
+      "name": "工具",
+      "order": 1,
+      "sites": []
     }
   ],
   "title": "猫猫导航🐱",
